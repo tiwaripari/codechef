c753aa5365ba0b4ba1b77979f0c4f9647e00dc64
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2021-11-21 15:07:17 +0530

    Add files via upload

---
+++ incrementanddecrement.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a%4==0)
+    {
+        cout<<a+1;
+    }
+    else
+    {
+        cout<<a-1;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
