9d31e55f5cb2f378dae1db9d302d291d0bf0a8f7
tiwaripari <34934229+tiwaripari@users.noreply.github.com>
2021-12-15 15:01:52 +0530

    Add files via upload

--- bases.cpp
+++ bases.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int main()
 {
-    int n,b,num,rem,num1=0,num2=0,temp,rem1;
+    int n,b,num,rem,num1=0,num2=0,num3=0,temp,rem1,count1=0,count2=0;
     cin>>n>>b;
     num=n;
     while(n>0)
     {
-
+        count1=count1+1;
         rem=num%b;
         num1=num1*10+rem;
         num=n/b;
         n=num;
     }
+
     while(num1>0)
     {
         rem1=num1%10;
@@ -20,7 +21,27 @@ int main()
         num2=num2*10+rem1;
         num1=temp;
     }
-     cout<<num2;
+
+    num3=num2;
+    while(num3>0)
+    {
+        temp=num3/10;
+        num3=temp;
+        count2=count2+1;
+    }
+
+    if(count2==count1)
+    {
+        cout<<num2;
+    }
+    if(count2<count1)
+    {
+        cout<<num2;
+        for(int i=0;i<(count1-count2);i++)
+        {
+            cout<<"0";
+        }
+    }
 
     return 0;
 
